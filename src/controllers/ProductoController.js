13e765d8e3baad03a274677b7af84796c39f340c
William Alonzo <williamalonzo4519@gmail.com>
2024-10-07 23:06:54 -0600

    Se agrego reporte pdf

--- src/controllers/ProductoController.js
+++ src/controllers/ProductoController.js
@@ -5,6 +5,7 @@ const Productos = require("../models/Productos");
 const path = require("path");
 const fs = require("fs");
 const multer = require("multer");
+const PDFDocument = require("pdfkit");
 
 router.post("/producto/register", async (req, res, next) => {
   try {
@@ -97,6 +98,75 @@ router.get("/producto/:id", async (req, res, next) => {
   } catch (error) {
     console.error("Error al obtener el paciente por ID:", error);
     res.status(500).send("Error interno del servidor");
+  }
+});
+
+router.get("/producto/generate/pdf", async (req, res, next) => {
+  // Crea un nuevo documento PDF
+  const doc = new PDFDocument({ margin: 30 });
+
+  // Configura los encabezados para mostrar el PDF en el navegador
+  res.setHeader("Content-Type", "application/pdf");
+  res.setHeader(
+    "Content-Disposition",
+    "inline; filename=reporte_productos.pdf"
+  );
+
+  // Enviar el documento directamente a la respuesta
+  doc.pipe(res);
+
+  // Título del reporte
+  doc.fontSize(20).text("Reporte de Productos", { align: "center" });
+  doc.moveDown(2); // Espacio después del título
+
+  try {
+    // Obtén los productos de la base de datos
+    const productos = await Productos.findAll({
+      where: { estado: true },
+    });
+
+    // Definir encabezados de la tabla
+    const tableTop = 100;
+    const itemHeight = 20;
+
+    // Dibujar encabezados de la tabla
+    doc.fontSize(10);
+    doc.text("ID", 50, tableTop);
+    doc.text("Nombre", 75, tableTop);
+    doc.text("Precio", 200, tableTop);
+    doc.text("Existencia", 300, tableTop);
+    doc.text("Fecha de Vencimiento", 375, tableTop);
+    doc.text("Estado", 500, tableTop);
+
+    doc
+      .moveTo(50, tableTop + 15)
+      .lineTo(550, tableTop + 15)
+      .stroke();
+
+    // Dibujar los datos de los productos en la tabla
+    let position = tableTop + 30;
+    productos.forEach((producto) => {
+      doc.text(producto.id, 50, position);
+      doc.text(producto.nombre, 75, position);
+      doc.text(`Q${producto.precio}`, 200, position);
+      doc.text(producto.existencia, 300, position);
+      doc.text(producto.fechaVencimiento.toDateString(), 375, position);
+      doc.text(producto.estado ? "Activo" : "Inactivo", 500, position);
+
+      // Dibujar línea separadora entre filas
+      doc
+        .moveTo(50, position + 15)
+        .lineTo(550, position + 15)
+        .stroke();
+
+      position += itemHeight; // Incrementar posición para la siguiente fila
+    });
+
+    // Finaliza el documento
+    doc.end();
+  } catch (error) {
+    console.error("Error al generar el reporte:", error);
+    res.status(500).send("Error al generar el reporte.");
   }
 });
 
